31f8ef25e0e90fba1ea24b7d3c5ebe90777a5a9e
Yoaltzin Xolalpa <urano.balam@gmail.com>
2022-10-29 00:40:03 -0400

    data base change

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-/*   "URI": "mongodb://127.0.0.1/books229" */
-//"URI": "mongodb://127.0.0.1/books"
-  "URI": "mongodb+srv://301184971:38H8Rg4P20KpDXJu@cluster0.kvvvtld.mongodb.net/?retryWrites=true&w=majority"
+/*   "URI": "mongodb://127.0.0.1/books229" prof uri */
+"URI": "mongodb://127.0.0.1/books"
+ // "URI": "mongodb+srv://301184971:38H8Rg4P20KpDXJu@cluster0.kvvvtld.mongodb.net/?retryWrites=true&w=majority"
 
 };
